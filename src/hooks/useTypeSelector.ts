141cb127c5744eb7b4cf9d88565fe2d7a41beef5
Remorselex <remorselex99@gmail.com>
2023-10-07 10:03:28 +0300

    feat: test commit

--- src/hooks/useTypeSelector.ts
+++ src/hooks/useTypeSelector.ts
@@ -1,4 +1,4 @@
 import { useSelector, TypedUseSelectorHook } from "react-redux";
 import { RootState } from "../store/reducers/combineReducer";
-
+//for commiot
 export const useTypeSelector: TypedUseSelectorHook<RootState> = useSelector;
